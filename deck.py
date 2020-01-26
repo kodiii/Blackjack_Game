86f81a8aaa0447aea2ccc2efaeaa383338b28c72
Ricardo <codzign@gmail.com>
2020-01-26 19:18:09 +0100

    create deck

---
+++ deck.py
@@ -0,0 +1,12 @@
+'''
+here its defined the deck
+'''
+
+class Deck():
+    # defining deck atributes
+    def __init__(self,suits,ranks):
+        self.suits = suits
+        self.ranks = ranks
+
+    
+        
