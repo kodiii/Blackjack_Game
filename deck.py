dc0dc4347e2f50d3d75e24a029192ce59628f6fb
Ricardo <codzign@gmail.com>
2020-02-02 18:51:01 +0100

    hand and chips classes

--- deck.py
+++ deck.py
@@ -8,25 +8,21 @@ suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
-# random_suits = suits[random.randrange(len(suits))]
-# random_ranks = ranks[random.randrange(len(ranks))]
-
-# print(random_ranks + ' ' + random_suits)
-
 playing = True
 
-class Cards():
+class Card():
+
     # defining card atributes
-    def __init__(self,suits,ranks):
-        self.suits = suits
-        self.ranks = ranks
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
 
     def __str__(self):
-        return self.ranks+ ' of ' +self.suits
+        return self.rank + ' of ' + self.suit
         
 class Deck():
-    # defining deck
-    def __init(self):
+    
+    def __init__(self):
         self.deck = []
         for suit in suits:
             for rank in ranks:
@@ -35,8 +31,8 @@ class Deck():
     def __str__(self):
         deck_comp = ''
         for card in self.deck:
-            deck_comp += '\n'+ card.__str__()
-        return 'The deck has: '+ deck_comp
+            deck_comp += '\n'+card.__str__()
+        return 'The deck has: '+deck_comp
 
     def shuffle(self):
         random.shuffle(self.deck)
@@ -45,5 +41,47 @@ class Deck():
         single_card = self.deck.pop()
         return single_card
 
-test_deck = Deck()
-print(test_deck)
+# test_deck = Deck()
+# test_deck.shuffle()
+# print(test_deck)
+
+class Hand():
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        # track Aces
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def adjust_ace(self):
+        while sel.value > 21 and self.aces > 0:
+            self.value -= 10
+            self.aces -= 1
+
+# test_deck = Deck()
+# test_deck.shuffle()
+
+# # player
+# test_player = Hand()
+# #deal 1 card from the deck Card(suit,rank)
+# pulled_card = test_deck.deal()
+# print(pulled_card)
+# test_player.add_card(pulled_card)
+# print(test_player.value)
+
+class Chips():
+    def __init__(self,total=100):
+        self.total = 100
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
